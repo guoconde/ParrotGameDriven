5c7362b9da807b8c4a758abb4e468273a6bc7550
guoconde <78751139+guoconde@users.noreply.github.com>
2021-11-07 16:17:47 -0300

    recomeçar e parabens adicionados

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-let qtdCartas = 14
+let qtdCartas = 4
 let nome = ''
 let primeiraCarta = ''
 let segundaCarta = ''
 let travaVirar = false
+let contador = 0
+let relogioSeg = 0
+let relogioMin = 0
+let relogioFunc = ''
+let novoJogo = ''
+
 
 const main = document.querySelector('#main')
 
+function relogio() {
+
+    relogioFunc = setInterval(() => {
+        relogioSeg++
+        if (relogioSeg < 10) {
+            relogioSeg = '0' + relogioSeg
+        }
+        if (relogioSeg > 59) {
+            relogioSeg = '00'
+            relogioMin++
+        }
+        if (relogioMin < 10) {
+            document.querySelector('section').innerHTML = `0${relogioMin}:${relogioSeg}`
+        } else {
+            document.querySelector('section').innerHTML = `${relogioMin}:${relogioSeg}`
+        }
+    }, 1000)
+}
+relogio()
 
 let cartas = [
     'bobrossparrot',
@@ -20,19 +45,20 @@ let cartas = [
 for (let i = 0; i < qtdCartas / 2; i++) {
 
     main.innerHTML += `
-    <div class="cartao" data-cartao="${cartas[i]}">
-    <img class="cartao-frente" src="./Assets/${cartas[i]}.gif">
-    <img class="cartao-verso" src="./Assets/front.png">
+    <div class="cartao" data-cartao="${cartas[i]}" data-identifier="card">
+    <img class="cartao-frente" src="./Assets/${cartas[i]}.gif" data-identifier="front-face">
+    <img class="cartao-verso" src="./Assets/front.png" data-identifier="back-face">
     </div>
-    <div class="cartao" data-cartao="${cartas[i]}">
-    <img class="cartao-frente" src="./Assets/${cartas[i]}.gif">
-    <img class="cartao-verso" src="./Assets/front.png">
+    <div class="cartao" data-cartao="${cartas[i]}" data-identifier="card">
+    <img class="cartao-frente" src="./Assets/${cartas[i]}.gif" data-identifier="front-face">
+    <img class="cartao-verso" src="./Assets/front.png" data-identifier="back-face">
     </div>
     `
 }
 
 function virarCarta() {
     if (travaVirar) return false
+    contador++
 
     this.classList.add('virar')
 
@@ -44,10 +70,12 @@ function virarCarta() {
     segundaCarta = this
 
     verificarIgualdade()
+    finalizarJogo()
 }
 
+const rand = document.querySelectorAll('.cartao')
+
 function aleatorio() {
-    const rand = document.querySelectorAll('.cartao')
 
     for (let i = 0; i < rand.length; i++) {
         let random = Math.floor(Math.random() * qtdCartas)
@@ -55,10 +83,51 @@ function aleatorio() {
         rand[i].style.order = random
     }
 
-
 }
 
 aleatorio()
+
+function finalizarJogo() {
+    const completa = document.querySelectorAll('.completa')
+
+    setTimeout(() => {
+        if (completa.length == qtdCartas) {
+
+            if (relogioMin < 1) {
+                alert(`Parabéns você ganhou com ${contador} jogadas. Com o tempo de: ${relogioSeg} segundos.`)
+                recomecar()
+            } else {
+                if (relogioMin < 2 && relogioMin >= 1) {
+                    alert(`Parabéns você ganhou com ${contador} jogadas. Com o tempo de: 0${relogioMin} minuto e ${relogioSeg} segundos.`)
+                    recomecar()
+                } else if (relogioMin < 10 && relogioMin >= 2) {
+                    alert(`Parabéns você ganhou com ${contador} jogadas. Com o tempo de: 0${relogioMin} minutos e ${relogioSeg} segundos.`)
+                    recomecar()
+                } else {
+                    alert(`Parabéns você ganhou com ${contador} jogadas. Com o tempo de: ${relogioMin} minutos e ${relogioSeg} segundos.`)
+                    recomecar()
+                }
+            }
+
+        }
+
+    }, 2000)
+
+    if (completa.length == qtdCartas) {
+        clearInterval(relogioFunc)
+    }
+
+}
+
+function recomecar() {
+    novoJogo = prompt('Gostaria de jogar novamente?')
+
+    if (novoJogo == 'sim' || novoJogo == 's' || novoJogo == 'yes' || novoJogo == '') {
+        window.location.reload()
+    } else {
+        alert('Ah que pena! Até logo.')
+    }
+}
 
 function verificarIgualdade() {
 
@@ -93,6 +162,8 @@ function limparVariaveis(igual = false) {
     segundaCarta = ''
 
 }
+
+
 
 
 const cartao = document.querySelectorAll('.cartao')
