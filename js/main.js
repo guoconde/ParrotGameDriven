837b5012057a81cdad80d06ec12eb62e3d22981e
guoconde <78751139+guoconde@users.noreply.github.com>
2021-11-07 14:41:30 -0300

    Jogo Completo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let qtdCartas = 14
 let nome = ''
+let primeiraCarta = ''
+let segundaCarta = ''
+let travaVirar = false
 
 const main = document.querySelector('#main')
 
@@ -17,16 +20,83 @@ let cartas = [
 for (let i = 0; i < qtdCartas / 2; i++) {
 
     main.innerHTML += `
-        <div class="cartao">
-            <img class="cartao-frente ${cartas[i]}" src="./Assets/${cartas[i]}.gif">
-            <img class="cartao-verso" src="./Assets/front.png">
-        </div>
-        <div class="cartao">
-            <img class="cartao-frente ${cartas[i]}" src="./Assets/${cartas[i]}.gif">
-            <img class="cartao-verso" src="./Assets/front.png">
-        </div>
-        `
+    <div class="cartao" data-cartao="${cartas[i]}">
+    <img class="cartao-frente" src="./Assets/${cartas[i]}.gif">
+    <img class="cartao-verso" src="./Assets/front.png">
+    </div>
+    <div class="cartao" data-cartao="${cartas[i]}">
+    <img class="cartao-frente" src="./Assets/${cartas[i]}.gif">
+    <img class="cartao-verso" src="./Assets/front.png">
+    </div>
+    `
 }
+
+function virarCarta() {
+    if (travaVirar) return false
+
+    this.classList.add('virar')
+
+    if (primeiraCarta == '') {
+        primeiraCarta = this
+
+        return false
+    }
+    segundaCarta = this
+
+    verificarIgualdade()
+}
+
+function aleatorio() {
+    const rand = document.querySelectorAll('.cartao')
+
+    for (let i = 0; i < rand.length; i++) {
+        let random = Math.floor(Math.random() * qtdCartas)
+
+        rand[i].style.order = random
+    }
+
+
+}
+
+aleatorio()
+
+function verificarIgualdade() {
+
+    let igual = primeiraCarta.dataset.cartao === segundaCarta.dataset.cartao
+
+    !igual ? desabilitarVirar() : limparVariaveis(igual)
+}
+
+function desabilitarVirar() {
+    travaVirar = true
+
+    setTimeout(() => {
+        primeiraCarta.classList.remove('virar')
+        segundaCarta.classList.remove('virar')
+
+        limparVariaveis()
+    }, 1000)
+}
+
+function limparVariaveis(igual = false) {
+
+    if (igual) {
+        primeiraCarta.removeEventListener('click', virarCarta)
+        primeiraCarta.classList.add('completa')
+
+        segundaCarta.removeEventListener('click', virarCarta)
+        segundaCarta.classList.add('completa')
+    }
+
+    travaVirar = false
+    primeiraCarta = ''
+    segundaCarta = ''
+
+}
+
+
+const cartao = document.querySelectorAll('.cartao')
+cartao.forEach(el => el.addEventListener('click', virarCarta))
 
 // for (let i = 0; i < qtdCartas / 2; i++) {
 
@@ -56,4 +126,3 @@ for (let i = 0; i < qtdCartas / 2; i++) {
 
 //     createGame()
 // }
-
